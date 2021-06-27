a4df22a6cbdc3cb5bfa26a8b856a67c9d5b4a476
Erick Cesar <e.cesar88@gmail.com>
2021-06-27 00:48:40 -0300

    Created ProductCardContainer

---
+++ client/src/components/ProductCardContainer.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const ProductCardWrapper = styled.div`
+  background-color: #f0f0f0;
+  width: 200px;
+  border-radius: 8px;
+  padding: 10px;
+  box-shadow: 0px 0px 5px #00000060;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+export default ProductCardWrapper
---
+++ client/src/contracts/ProductCardProps.ts
@@ -0,0 +1,9 @@
+interface ProductCardProps {
+  id: number
+  name: string
+  price: number
+  score: number
+  image?: string
+}
+
+export default ProductCardProps

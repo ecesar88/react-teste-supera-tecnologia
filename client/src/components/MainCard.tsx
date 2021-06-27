b9c69fbcf85d9d9bd2dbeb856f277a12f1b7a8c5
Erick Cesar <e.cesar88@gmail.com>
2021-06-27 00:48:10 -0300

    Created maincard component

---
+++ client/src/components/MainCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled, { CSSProperties } from 'styled-components'
+
+const MainCard = (): JSX.Element => {
+  const style: { [key: string]: CSSProperties } = {}
+
+  return (
+    <div style={{}}>
+      <p></p>
+    </div>
+  )
+}
+
+export default MainCard

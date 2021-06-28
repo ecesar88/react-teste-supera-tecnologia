2b5462979c29c0fce61a28ecccf2054fb6eae2a8
Erick Cesar <e.cesar88@gmail.com>
2021-06-27 23:30:00 -0300

    Extra button functionality to the cart button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -28,4 +28,54 @@ const Button = styled.button`
   }
 `
 
-export default Button
+const CartButton = styled.button<ButtonProps & { cart: number }>`
+  position: relative;
+
+  display: inline-block;
+  background-color: ${(props: ButtonProps) =>
+    props.backgroundColor ? props.backgroundColor : '#d41f1f'};
+  border-radius: 5px;
+  border: 4px #cccccc;
+  color: ${(props: ButtonProps) => props.color ?? '#eeeeee'};
+  text-align: center;
+  font-size: 16px;
+  padding: 5px;
+  width: 90px;
+  -webkit-transition: all 0.5s;
+  -moz-transition: all 0.5s;
+  -o-transition: all 0.5s;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 3px;
+
+  &:hover {
+    background-color: #9c0b0b;
+  }
+
+  &:active {
+    background-color: #690808;
+  }
+
+  ${(props) =>
+    props.cart &&
+    `
+  ::after {
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: yellow;
+    color: black;
+    border-radius: 100%;
+    font-size: 10px;
+    width: 18px;
+    height: 18px;
+    top: -8px;
+    right: -8px;
+    content: '${props.cart}';
+  }
+  `}
+`
+
+export { Button, CartButton }
